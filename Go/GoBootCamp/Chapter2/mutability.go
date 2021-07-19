51ea94963d1e7795e15db0b5db06ceb804571aba
Jason <jason@silph.dev>
2021-07-19 15:02:23 -0700

    added gitignore and changed naming

--- Go/GoBootCamp/Chapter2/mutability.go
+++ Go/GoBootCamp/Chapter2/mutability.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// Only constants are immutable in Go. Original values will also not be mutated when being passed in arguments/functions.
+
 func main() {
 	me := Artist{Name: "Matt", Genre: "Electro", Songs: 42}
 	fmt.Printf("%s released their %dth song\n", me.Name, newRelease(me))
